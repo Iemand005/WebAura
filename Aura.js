d29bc4658aed8092a8d740b946e99385b8cc52f4
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-02 14:53:13 +0200

    Move functions into it

--- Aura.js
+++ Aura.js
@@ -1,16 +1,14 @@
-const devices = await navigator.hid.requestDevice({
-  filters: [{ vendorId: 0x0B05 }]
-});
-
-devices.forEach(device => {
-    device.collections.forEach(collection => {
-        console.log(collection.usagePage == 0xFF31)
-    });
-});
-
 class Aura {
 
 	async getAuraDevice() {
-		
+		const devices = await navigator.hid.requestDevice({
+		filters: [{ vendorId: 0x0B05 }]
+		});
+
+		devices.forEach(device => {
+			device.collections.forEach(collection => {
+				console.log(collection.usagePage == 0xFF31)
+			});
+		});
 	}
 }
