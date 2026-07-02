c199a60772474f63f741172e8dcb1a4213129c1f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-02 14:41:44 +0200

    Add basic HID API testing

---
+++ Aura.js
@@ -0,0 +1,9 @@
+const devices = await navigator.hid.requestDevice({
+  filters: [{ vendorId: 0x0B05 }]
+});
+
+devices.forEach(device => {
+    device.collections.forEach(collection => {
+        console.log(collection.usagePage == 0xFF31)
+    });
+});
