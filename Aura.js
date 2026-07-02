3806d0928881651692c3ad7dc0b150399903828c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-02 15:14:02 +0200

    aura init reproting

--- Aura.js
+++ Aura.js
@@ -13,4 +13,20 @@ class Aura {
 
 		if (device) this.device = device;
 	}
+
+	async sendAuraInitReport() {
+		if (!this.device) return;
+		const buffer = new ArrayBuffer(63); 
+		const view = new DataView(buffer);
+
+		view.setUint8(0, 0xBC); // cmd (byte 1)
+		view.setUint8(1, 0x01); // mode (byte 2)
+
+
+		const reportId = 0x5A; // byte 0
+		const data = new Uint8Array(buffer);
+
+		await this.device.sendReport(reportId, data);
+	}
+
 }
